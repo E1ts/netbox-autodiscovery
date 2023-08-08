4d3a712acb8048d9d69687dd568a64103c58c82e
e1ts <e.jernkrook@gmail.com>
2023-08-08 16:30:58 +0200

    Added log function

--- scanner.py
+++ scanner.py
@@ -2,8 +2,9 @@ import os
 import platform
 import ipaddress
 import requests
-from datetime import datetime
+import logging
 
+from datetime import datetime
 from dotenv import load_dotenv
 
 # Load the environment variables from the .env file
@@ -12,11 +13,34 @@ load_dotenv()
 # Access the variables using os.environ
 NETBOX_URL = os.environ.get("NETBOX_URL")
 NETBOX_TOKEN = os.environ.get("NETBOX_TOKEN")
+NETBOX_TAG_NAME = os.environ.get("NETBOX_TAG_NAME")
 
 
-# Add subnet to scan
-ip_subnet = "192.168.1.0/24"
 
+# Setup Logging
+logging.basicConfig(filename='scanner.log', level=logging.DEBUG)
+
+
+
+# Suppress InsecureRequestWarning
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def get_prefixes_with_tag(api_url, api_token, tag_name):
+    headers = {
+        "Authorization": f"Token {api_token}"
+    }
+    
+    # Hämta alla prefixes med den specifika taggen
+    response = requests.get(f"{NETBOX_URL}/api/ipam/prefixes/?tag={tag_name}", headers=headers, verify=False)
+    
+    if response.status_code == 200:
+        prefixes_data = response.json()
+        prefixes = [prefix['prefix'] for prefix in prefixes_data['results']]
+        return prefixes
+    else:
+        print(f"Fel vid hämtning av prefixes: {response.text}")
+        return []
 
 def get_ip_from_subnet(ip_subnet):
     ips = ipaddress.ip_network(ip_subnet)
@@ -35,17 +59,16 @@ def check_ip_in_netbox(ip_address):
         "address": ip_address,
     }
 
-    response = requests.get(NETBOX_URL, params=params, headers=headers, verify=False)
+    response = requests.get(f"{NETBOX_URL}/api/ipam/ip-addresses/", params=params, headers=headers, verify=False)
 
     if response.status_code == 200:
         try:
             data = response.json()
             if data.get("count", 0) > 0:
-                print('IP address already exists in NetBox')
                 return True  # IP address already exists in NetBox
         except ValueError:
             # If the response is not JSON, handle it here (e.g., print the response)
-            print("Response is not in JSON format:", response.text)
+            logging.error(f"Response is not in JSON format:  {response.text}")
     return False
 
 def add_ip_to_netbox(ip_address):
@@ -62,42 +85,44 @@ def add_ip_to_netbox(ip_address):
             # Additional fields like "description", "tags", etc., can be included here
         }
 
-        response = requests.post(NETBOX_URL, json=data, headers=headers, verify=False)
+        response = requests.post(f"{NETBOX_URL}/api/ipam/ip-addresses/", json=data, headers=headers, verify=False)
 
         if response.status_code == 201:
-            print(f"Added {ip_address} to NetBox.")
+            logging.info(f"Added {ip_address} to NetBox")
         else:
-            print(f"Failed to add {ip_address} to NetBox.")
+            logging.error(f"Failed to add {ip_address} to NetBox.")
     else:
         print(f"{ip_address} already exists in NetBox.")
 
+prefixes = get_prefixes_with_tag(f"{NETBOX_URL}/api/ipam/ip-addresses/", NETBOX_TOKEN, NETBOX_TAG_NAME)
 
+for ip_subnet in prefixes:
+    print(ip_subnet)
+    ips = get_ip_from_subnet(ip_subnet)
 
-ips = get_ip_from_subnet(ip_subnet)
+    oper = platform.system()
 
-oper = platform.system()
+    if (oper == "Windows"):
+        ping1 = "ping -n 1 "
+    elif (oper == "Linux"):
+        ping1 = "ping -c 1 "
+    else:
+        ping1 = "ping -c 1 "
 
-if (oper == "Windows"):
-    ping1 = "ping -n 1 "
-elif (oper == "Linux"):
-    ping1 = "ping -c 1 "
-else:
-    ping1 = "ping -c 1 "
+    t1 = datetime.now()
+    print("Scanning in Progress:")
 
-t1 = datetime.now()
-print("Scanning in Progress:")
+    for addr in ips:
+        print(addr)
+        comm = ping1 + addr
+        response = os.popen(comm)
 
-for addr in ips:
-    print(addr)
-    comm = ping1 + addr
-    response = os.popen(comm)
-
-    for line in response.readlines():
-        if "ttl" in line.lower():
-            # Add the IP address to NetBox if it responds to ping
-            add_ip_to_netbox(addr)
-            break
+        for line in response.readlines():
+            if "ttl" in line.lower():
+                # Add the IP address to NetBox if it responds to ping
+                add_ip_to_netbox(addr)
+                break
 
 t2 = datetime.now()
 total = t2 - t1
-print("Scanning completed in: ", total)
+logging.error("Scanning completed in: {total}")
